6ec88c0887f723d7e6a689b771e98cfc96c418a0
Arthur Batista Cabral <abcabral827@gmail.com>
2025-09-18 21:18:44 -0300

    commit 2

---
+++ algoritimo_idade/idade_C#/idade/Program.cs
@@ -0,0 +1,14 @@
+﻿
+string nome;
+int idade = 0;
+int data;
+Console.WriteLine("Digite seu nome");
+nome = Convert.ToString(Console.ReadLine());
+
+Console.WriteLine("Digite seu ano de nascimento");
+data =  Convert.ToInt32 (Console.ReadLine());
+idade += 2025 - data;
+Console.WriteLine("Sua idade: " + Convert.ToString(idade));
+Console.WriteLine("Seu nome: " + Convert.ToString(nome));
+
+
